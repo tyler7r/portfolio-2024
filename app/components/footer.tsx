5fbfaccd9572264054c01749908051229dbc3098
Tyler Randall <tyler7r@gmail.com>
2024-08-25 16:40:56 -0400

    Remove css styles from globals.css

---
+++ app/components/footer.tsx
@@ -0,0 +1,10 @@
+import { Divider } from "@mui/material";
+
+const Footer = () => {
+  <div className="w-full flex">
+    <Divider style={{ width: "100%" }} />
+    <div></div>
+  </div>;
+};
+
+export default Footer;
