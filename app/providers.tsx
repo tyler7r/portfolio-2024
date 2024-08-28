71a4692c763f2fcee1505bc4870ae5c104623dcf
Tyler Randall <tyler7r@gmail.com>
2024-08-28 16:38:48 -0400

    Add contact component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import { ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { IsMobile } from "./contexts/mobile";
+import theme from "./contexts/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <IsMobile>{children}</IsMobile>;
+  return (
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <IsMobile>{children}</IsMobile>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
 }
